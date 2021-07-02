ebe719b14e02fdf93bd1a5cfd5991cb1edf47539
Adamova Galina <adamova@triya.ru>
2021-07-02 15:53:45 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import requests
+
+from pprint import pprint
+
+token = ' '
+
+class YandexDisk:
+    def __init__(self, token):
+        self.token = token
+
+    def get_headers(self):
+        return {
+            'Content-type': 'application/json',
+            'Authorization': 'OAuth {}'.format(self.token)
+        }
+
+    def get_files_list(self):
+        files_url = 'https://cloud-api.yandex.net/v1/disk/resources/files'
+        headers = self.get_headers()
+        response = requests.get(files_url, headers=headers)
+        return response.json()
+
+    def _get_upload_link(self, disk_file_path):
+        upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+        headers = self.get_headers()
+        params = {'path': disk_file_path, 'overwrite': 'true'}
+        response = requests.get(upload_url, headers=headers, params=params)
+        pprint(response.json())
+        return response.json()
+
+    def upload_file_disk(self, disk_file_path, file_name):
+        href = self._get_upload_link(disk_file_path=disk_file_path).get('href','')
+        response = requests.put(href, data=open(file_name, 'rb'))
+        response.raise_for_status()
+        if response.status_code == 201:
+            print('Файл загружен на YandexDisk')
+
+yd = YandexDisk(token=token)
+yd.upload_file_disk(disk_file_path='Requests_http_запросы/Requests_http_запросы.txt', file_name='Requests_http_запросы.txt')
+
+
+
+
+
